0d6f580019ca854bcd5406a32f8ea9b087553154
Fabio Copa Mejia <64265042+fabi-cm@users.noreply.github.com>
2022-05-31 12:08:04 -0400

    pruebas

--- src/sumador.js
+++ src/sumador.js
@@ -5,4 +5,25 @@ function sumar(a, b) {
 function cambio(a, b) {
   return a - b;
 }
+
+function cantidadMonedas(a) {
+  let monedas = [5,2,1,0.5,0.2];
+  let cambioDevolver = [];
+  let monto = a;
+  let i = 0;
+  while (monto <= 0) {
+    monto -= cambioDe(monto, monedas[i]);
+    cambioDevolver.push(cambioDe(monto, monedas[i]));
+    i++;
+  }
+  return cambioDevolver;
+}
+
+function cambioDe(a, moneda) {
+  if (a > moneda) {
+    return moneda;
+  } else {
+    return 0;
+  }
+}
 export default sumar;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -5,3 +5,10 @@ describe("Sumar", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 });
+
+// pruebas TDD:
+describe("Monedas", () => {
+  if("Deberia devolver una lista de monedas:", () => {
+    expect(cantidadMonedas(19)).toEqual([5,5,5,2,2]);
+  });
+});
